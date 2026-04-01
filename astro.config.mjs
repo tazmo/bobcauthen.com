be05992a5be73e2f7ba91e0c23cc64ec32114a17
Bob Cauthen <4428450+tazmo@users.noreply.github.com>
2026-04-01 10:26:54 -0400

    lots of updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import siteConfig from './src/data/site-config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [mdx(), sitemap()]
+  site: siteConfig.website,
+
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare()
 });
